c08fa8878142bb4263534d9509a7b9a24c7667e0
HELEN-DEE <helendeee12@gmail.com>
2024-01-12 21:51:09 +0100

    added Functionality and Responsiveness

--- script.js
+++ script.js
@@ -2,8 +2,23 @@ const menu = document.getElementById('menu')
 const headerMenu = document.getElementById('headerMenu')
 const header = document.getElementById('header')
 
-menu.addEventListener('click', () => {
+let marginToggle = false;
+let isExpanded = false;
+
+menu.addEventListener('click', (event) => {
+    event.stopPropagation()
+
     menu.classList.toggle('open')
     headerMenu.classList.toggle('open-menu')
-    header.classList.toggle('header-black')
+    header.style.backgroundColor = header.style.backgroundColor === 'black' ? 'transparent' : 'black';
+
+    marginToggle = !marginToggle;
+    header.style.marginLeft = marginToggle ? '-1.3rem' : '0';
+    header.style.marginRight = marginToggle ? '-1.3rem' : '0';
+
+    isExpanded = !isExpanded;
+    headerMenu.style.height = isExpanded ? '90vh' : 'auto';
+    header.style.paddingLeft = header.style.paddingRight = isExpanded ? '20px' : '0';
+    header.style.marginTop = isExpanded ? '-2rem' : '0'; 
+    header.style.paddingTop = isExpanded ? '2rem' : '0'; 
 })
